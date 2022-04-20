8b19e8886d675cacd6d54a07df62141ab809e084
christinayau1 <100316206+christinayau1@users.noreply.github.com>
2022-04-20 16:17:28 -0500

    Update scripts.js
    - add function to show and hide menu bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
 alert("Hello! Welcome to my Portfolio Site!");
 console.log("Hello! Welcome to my Portfolio Site!");
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.classname === 'navtoggle') {
+        x.className += ' responsive';
+    }else {
+        x.className = 'navtoggle';
+    }
+}
