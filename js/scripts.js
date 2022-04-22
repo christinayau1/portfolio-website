05330fdf4c54f4837882f59edfe02e277cefb3fc
christinayau1 <100316206+christinayau1@users.noreply.github.com>
2022-04-22 02:35:49 -0500

    Update scripts.js
    - remove window alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
-alert("Hello! Welcome to my Portfolio Site!");
-console.log("Hello! Welcome to my Portfolio Site!");
 function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.classname === 'navtoggle') {
-        x.className += ' responsive';
-    }else {
-        x.className = 'navtoggle';
-    }
+  var x = document.getElementById('myNavtoggle');
+  if (x.classname === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
 }
